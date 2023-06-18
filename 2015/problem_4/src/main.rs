423d3f01086c62aaa7df9f4f2b25b0d0e5f19d3a
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2023-06-18 23:02:58 +0300

    read me and simplification for problem 4

--- 2015/problem_4/src/main.rs
+++ 2015/problem_4/src/main.rs
@@ -1,32 +1,33 @@
 use md5::{compute, Digest};
 
+// NOTE: run in release mode. much slower in debug mode.
+
 fn main() {
     let input = "iwrupvqb";
-    let mut n = 1;
+    let mut counter = 1;
 
-    loop {
-        let hash = compute(format!("{}{}", input, n));
+    let mut calc_hash_until_cond = |predicate: &(dyn Fn(Digest) -> bool)| loop {
+        let hash = compute(format!("{}{}", input, counter));
 
-        if starts_with_five_zeroes(hash) {
-            println!("The answer of part 1 is {}", n); // 346386
-            break;
+        if predicate(hash) {
+            return counter;
         }
 
-        n += 1;
-    }
+        counter += 1;
+    };
 
-    loop {
-        let hash = compute(format!("{}{}", input, n));
+    println!(
+        "The answer of part 1 is {}",
+        calc_hash_until_cond(&starts_with_five_zeroes)
+    ); // 346386
 
-        if starts_with_six_zeroes(hash) {
-            println!("The answer of part 2 is {}", n); // 9958218
-            break;
-        }
-
-        n += 1;
-    }
+    println!(
+        "The answer of part 2 is {}",
+        calc_hash_until_cond(&starts_with_six_zeroes)
+    ); // 9958218
 }
 
+// Digest is essentially an array [u8; 16]
 fn starts_with_five_zeroes(digest: Digest) -> bool {
     let result = digest.0;
 
