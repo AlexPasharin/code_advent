857785ebf97925b89a4159e733b9430ef9022edd
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2023-06-18 16:02:05 +0300

    problem 3 README, code simplifications and comments

--- 2015/problem_3/src/main.rs
+++ 2015/problem_3/src/main.rs
@@ -1,28 +1,18 @@
 use std::collections::HashSet;
 use utils::file_reader::FileReader;
 
-#[derive(PartialEq, Eq, Hash, Clone)]
-struct Point(i32, i32);
-
-impl Point {
-    fn new(x: i32, y: i32) -> Self {
-        Self(x, y)
-    }
-}
-
 fn main() {
-    let mut p1 = Point::new(0, 0);
+    let origin = (0, 0);
 
-    let mut points1 = HashSet::new();
-    points1.insert(p1.clone());
+    // since Point is copy, these are all independent copies
+    let mut p1 = origin; // tracks position of Santa in part 1
+    let mut p2 = origin; // tracks position of Santa in part 2
+    let mut p3 = origin; // tracks position of RoboSanta in part 2
 
-    let mut p2 = Point::new(0, 0);
-    let mut p3 = Point::new(0, 0);
+    let mut points1 = HashSet::from([p1]); // set of points visited by Santa in part 1
+    let mut points2 = HashSet::from([p2]); // set of points visited by Santa and RoboSanta in part 2
 
-    let mut points2 = HashSet::new();
-    points2.insert(p2.clone());
-
-    let mut santa_turn = true;
+    let mut santa_turn = true; // for part 2, true if Santa turn, RoboSanta turn otherwise
 
     FileReader::process_file("./input.txt", &mut |ch| {
         let mut second_year_point = if santa_turn { &mut p2 } else { &mut p3 };
@@ -44,11 +34,17 @@ fn main() {
                 p1.1 -= 1;
                 second_year_point.1 -= 1;
             }
-            _ => {}
+            _ => (), // this is not needed logically, but match in Rust be exustive. Perhaps good old fashioned if else would be better in this case
         }
 
-        points1.insert(p1.clone());
-        points2.insert(second_year_point.clone());
+        // this works because (i32, i32) is a copy type, so every time we insert it, we actually insert a copy
+        points1.insert(p1);
+        // second_year_point on the other hand must be a reference to either p2 or p3, coz otherwise second_year_point will always be a cloned copy of p2 or p3, so any modifications of it wont affect them
+        points2.insert(*second_year_point);
+
+        // NOTE: the approach taken, leading to assymetry above (p1 can be value, but second_year_point must be a reference) is left because its a good exercise in understaing Rust copy-move-references mechanics
+        // however it had lead to clearly smelly code
+        // since we are doing (implicit) copying anyway, better approach stylistically would be to use immutable Point objects
 
         santa_turn = !santa_turn;
     });
