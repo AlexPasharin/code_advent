e13040c7b71ff42d471eafa346e1478a3d12b933
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2023-06-19 09:00:39 +0300

    problem 5 readme

--- 2015/problem_5/src/main.rs
+++ 2015/problem_5/src/main.rs
@@ -10,69 +10,69 @@ fn main() {
 
         let prev = chars.get(0);
 
-        // empty line case
-        if prev == None {
-            return;
-        }
+        match prev {
+            None => return,
+            Some(prev) => {
+                let mut prev = *prev;
 
-        let mut prev = *prev.unwrap(); // first char in the string
+                let mut amount_of_vowels = if is_vowel(prev) { 1 } else { 0 };
+                let mut has_consecutive_dublicates = false;
+                let mut contains_forbidden_pair = false;
 
-        let mut amount_of_vowels = if is_vowel(prev) { 1 } else { 0 };
-        let mut has_consecutive_dublicates = false;
-        let mut contains_forbidden_pair = false;
+                let mut contains_pair_twice = false;
 
-        let mut contains_pair_twice = false;
+                let mut different_letter_pairs: HashSet<[char; 2]> = HashSet::new();
+                let mut same_letter_twice: HashMap<char, usize> = HashMap::new();
 
-        let mut different_letter_pairs: HashSet<String> = HashSet::new();
-        let mut same_letter_twice: HashMap<char, usize> = HashMap::new();
+                let mut contains_letter_between_same_letters = false;
 
-        let mut contains_letter_between_same_letters = false;
+                for current_index in 1..chars.len() {
+                    let curr = *chars.get(current_index).unwrap();
 
-        for current_index in 1..chars.len() {
-            let curr = *chars.get(current_index).unwrap();
+                    if is_vowel(curr) {
+                        amount_of_vowels += 1;
+                    }
 
-            if is_vowel(curr) {
-                amount_of_vowels += 1;
-            }
+                    if prev == curr {
+                        has_consecutive_dublicates = true;
 
-            if prev == curr {
-                has_consecutive_dublicates = true;
+                        if let Some(prev_same_letter_twice_index) = same_letter_twice.get(&prev) {
+                            contains_pair_twice = contains_pair_twice
+                                || (*prev_same_letter_twice_index < current_index - 1);
+                        } else {
+                            same_letter_twice.insert(curr, current_index);
+                        }
+                    } else {
+                        let consecutive_pair = [prev, curr];
 
-                if let Some(prev_same_letter_twice_index) = same_letter_twice.get(&prev) {
-                    contains_pair_twice =
-                        contains_pair_twice || (*prev_same_letter_twice_index < current_index - 1);
-                } else {
-                    same_letter_twice.insert(curr, current_index);
+                        if different_letter_pairs.contains(&consecutive_pair) {
+                            contains_pair_twice = true;
+                        } else {
+                            different_letter_pairs.insert(consecutive_pair);
+
+                            contains_forbidden_pair =
+                                contains_forbidden_pair || char_pair_is_forbidden(prev, curr)
+                        }
+                    }
+
+                    if current_index > 1 {
+                        let prev_prev = *chars.get(current_index - 2).unwrap();
+
+                        contains_letter_between_same_letters =
+                            contains_letter_between_same_letters || prev_prev == curr
+                    }
+
+                    prev = curr;
                 }
-            } else {
-                let consecutive_pair = format!("{prev}{curr}");
 
-                if different_letter_pairs.contains(&consecutive_pair) {
-                    contains_pair_twice = true;
-                } else {
-                    different_letter_pairs.insert(consecutive_pair);
+                if amount_of_vowels > 2 && has_consecutive_dublicates && !contains_forbidden_pair {
+                    amount_of_nice_strings_part_one += 1;
+                }
 
-                    contains_forbidden_pair =
-                        contains_forbidden_pair || char_pair_is_forbidden(prev, curr)
+                if contains_pair_twice && contains_letter_between_same_letters {
+                    amount_of_nice_strings_part_two += 1;
                 }
             }
-
-            if current_index > 1 {
-                let prev_prev = *chars.get(current_index - 2).unwrap();
-
-                contains_letter_between_same_letters =
-                    contains_letter_between_same_letters || prev_prev == curr
-            }
-
-            prev = curr;
-        }
-
-        if amount_of_vowels > 2 && has_consecutive_dublicates && !contains_forbidden_pair {
-            amount_of_nice_strings_part_one += 1;
-        }
-
-        if contains_pair_twice && contains_letter_between_same_letters {
-            amount_of_nice_strings_part_two += 1;
         }
     });
 
