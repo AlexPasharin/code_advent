91be6105d512718a0b4f25511b00b82cd4e33283
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2023-07-15 17:20:22 +0300

    problems 16 and 17

---
+++ 2015/src/bin/problem_16/main.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+use utils::file_reader::FileReader;
+
+fn main() {
+    let re = Regex::new(
+      r"^Sue (\d+): ((?:(?:children|cats|samoyeds|pomeranians|akitas|vizslas|goldfish|trees|cars|perfumes): \d+, )+(?:(?:children|cats|samoyeds|pomeranians|akitas|vizslas|goldfish|trees|cars|perfumes): \d+))$",
+  )
+  .unwrap();
+
+    let suitable_aunt_properties = HashMap::from([
+        ("children", 3),
+        ("cats", 7),
+        ("samoyeds", 2),
+        ("pomeranians", 3),
+        ("akitas", 0),
+        ("vizslas", 0),
+        ("goldfish", 5),
+        ("trees", 3),
+        ("cars", 2),
+        ("perfumes", 1),
+    ]);
+
+    let mut part_1_suitable_aunt_numbers = Vec::new();
+    let mut part_2_suitable_aunt_numbers = Vec::new();
+
+    FileReader::process_lines("./input/problem_16.txt", &mut |line| {
+        let capture_match = re
+            .captures(line.trim())
+            .expect(&(String::from("Could not match the input line ") + line));
+
+        let aunt_number = capture_match[1].parse::<u32>().unwrap();
+        let aunt_properties = capture_match[2]
+            .split(", ")
+            .map(|prop| prop.split(": ").collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        let part_1_aunt_is_suitable = aunt_properties.iter().all(|property_spec| {
+            let property_name = property_spec[0];
+            let amount = property_spec[1].parse::<u32>().unwrap();
+
+            *suitable_aunt_properties.get(property_name).unwrap() == amount
+        });
+
+        let part_2_aunt_is_suitable = aunt_properties.iter().all(|property_spec| {
+            let property_name = property_spec[0];
+            let amount = property_spec[1].parse::<u32>().unwrap();
+
+            if property_name == "cats" || property_name == "trees" {
+                amount > *suitable_aunt_properties.get(property_name).unwrap()
+            } else if property_name == "pomeranians" || property_name == "goldfish" {
+                amount < *suitable_aunt_properties.get(property_name).unwrap()
+            } else {
+                *suitable_aunt_properties.get(property_name).unwrap() == amount
+            }
+        });
+
+        if part_1_aunt_is_suitable {
+            part_1_suitable_aunt_numbers.push(aunt_number);
+        }
+
+        if part_2_aunt_is_suitable {
+            part_2_suitable_aunt_numbers.push(aunt_number);
+        }
+    });
+
+    println!("Suitable aunts, part 1: {:?}", part_1_suitable_aunt_numbers); // [40]
+    println!("Suitable aunts, part 2: {:?}", part_2_suitable_aunt_numbers); // [241]
+}
---
+++ 2015/src/bin/problem_17/main.rs
@@ -0,0 +1,79 @@
+use std::collections::VecDeque;
+use utils::file_reader::FileReader;
+
+struct ContainersSet {
+    indices_used: Vec<usize>,
+    size: u32,
+}
+
+const EGGNOG_AMOUNT: u32 = 150;
+
+fn main() {
+    let mut containers = Vec::new();
+
+    FileReader::process_lines("./input/problem_17.txt", &mut |line| {
+        let container_size = line.trim().parse::<u32>().unwrap();
+
+        containers.push(container_size);
+    });
+
+    assert!(containers.iter().all(|c| *c > 0));
+
+    let mut queue = VecDeque::new();
+    let amount_of_containers = containers.len();
+
+    for idx in 0..amount_of_containers {
+        queue.push_back(ContainersSet {
+            indices_used: vec![idx],
+            size: containers[idx],
+        });
+    }
+
+    let mut ways_to_get_eggnot_amount = 0;
+
+    let mut minimal_amount_of_containers_needed = usize::MAX;
+    let mut ways_to_get_eggnot_amount_with_minimal_amount = 0;
+
+    while let Some(ContainersSet { indices_used, size }) = queue.pop_front() {
+        if size == EGGNOG_AMOUNT {
+            ways_to_get_eggnot_amount += 1;
+
+            let current_amount_of_containers_used = indices_used.len();
+
+            if current_amount_of_containers_used == minimal_amount_of_containers_needed {
+                ways_to_get_eggnot_amount_with_minimal_amount += 1;
+            } else if current_amount_of_containers_used < minimal_amount_of_containers_needed {
+                minimal_amount_of_containers_needed = current_amount_of_containers_used;
+                ways_to_get_eggnot_amount_with_minimal_amount = 1;
+            }
+        }
+
+        if size >= EGGNOG_AMOUNT {
+            continue;
+        }
+
+        let max_index_used = indices_used.last().unwrap();
+
+        for i in (max_index_used + 1)..amount_of_containers {
+            let mut new_indiced_used = indices_used.clone();
+            new_indiced_used.push(i);
+
+            let new_size = size + containers[i];
+
+            queue.push_back(ContainersSet {
+                indices_used: new_indiced_used,
+                size: new_size,
+            });
+        }
+    }
+
+    println!(
+        "Part 1, ways to get exact eggnog amount: {}",
+        ways_to_get_eggnot_amount
+    ); // 1304
+
+    println!(
+        "Part 2, ways to get exact eggnog amount with minimal amount f containers: {}",
+        ways_to_get_eggnot_amount_with_minimal_amount
+    ); // 18
+}
