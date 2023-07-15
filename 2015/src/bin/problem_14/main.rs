da4b777889e25426d2504df3fba3e2e9bb84d6a2
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2023-07-15 12:19:05 +0300

    problems 14 and 15

---
+++ 2015/src/bin/problem_14/main.rs
@@ -0,0 +1,114 @@
+use std::cmp::min;
+
+use regex::Regex;
+
+use utils::file_reader::FileReader;
+
+const OVERALL_RUN_TIME: u32 = 2503;
+
+struct Reindeer {
+    // name: String,
+    speed: u32,
+    continuous_running_time: u32,
+    continuous_cycle_length: u32,
+    current_place_in_cycle: u32,
+    distance_ran: u32,
+    points: u32,
+}
+
+impl Reindeer {
+    fn next_state(&mut self) {
+        let Reindeer {
+            speed,
+            continuous_running_time,
+            continuous_cycle_length,
+            current_place_in_cycle,
+            distance_ran,
+            ..
+        } = self;
+
+        if current_place_in_cycle <= continuous_running_time {
+            *distance_ran += *speed;
+        }
+
+        if current_place_in_cycle == continuous_cycle_length {
+            *current_place_in_cycle = 1;
+        } else {
+            *current_place_in_cycle += 1;
+        }
+    }
+}
+
+fn main() {
+    let re = Regex::new(
+        r"^(?:\S+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.$",
+    )
+    .unwrap();
+
+    let mut max_distance = 0;
+
+    let mut reindeers: Vec<Reindeer> = Vec::new();
+
+    FileReader::process_lines("./input/problem_14.txt", &mut |line| {
+        let capture_match = re.captures(line.trim()).unwrap();
+
+        let parse_int = |idx: usize| capture_match[idx].parse::<u32>().unwrap();
+
+        let speed = parse_int(1);
+        let continuous_running_time = parse_int(2);
+        let rest_seconds = parse_int(3);
+
+        let continuous_cycle_length = continuous_running_time + rest_seconds;
+
+        reindeers.push(Reindeer {
+            speed,
+            continuous_running_time,
+            continuous_cycle_length,
+            current_place_in_cycle: 1,
+            distance_ran: 0,
+            points: 0,
+        });
+
+        let km_for_one_cycle = speed * continuous_running_time;
+        let amount_of_full_cycles = OVERALL_RUN_TIME / continuous_cycle_length;
+        let reminder = OVERALL_RUN_TIME % continuous_cycle_length;
+
+        let total_distance = amount_of_full_cycles * km_for_one_cycle
+            + min(reminder, continuous_running_time) * speed;
+
+        if total_distance > max_distance {
+            max_distance = total_distance;
+        }
+    });
+
+    println!("Part 1: Longest distance is {}", max_distance); // 2655
+
+    for _ in 0..OVERALL_RUN_TIME {
+        let mut max_distance = 0u32;
+        let mut max_distance_reindeers = Vec::new();
+
+        for reindeer in &mut reindeers {
+            reindeer.next_state();
+
+            if reindeer.distance_ran > max_distance {
+                max_distance = reindeer.distance_ran;
+                max_distance_reindeers = vec![reindeer];
+            } else if reindeer.distance_ran == max_distance {
+                max_distance_reindeers.push(reindeer);
+            }
+        }
+
+        for reindeer in max_distance_reindeers {
+            reindeer.points += 1;
+        }
+    }
+
+    let max_distance = reindeers.iter().map(|r| r.distance_ran).max().unwrap();
+    let max_points = reindeers.iter().map(|r| r.points).max().unwrap();
+
+    println!(
+        "Part 1: Longest distance, calculated another way: {}",
+        max_distance
+    ); // 2655
+    println!("Part 2: Max points: {}", max_points); // 1059
+}
---
+++ 2015/src/bin/problem_15/main.rs
@@ -0,0 +1,151 @@
+use std::cmp::max;
+
+use regex::Regex;
+use utils::file_reader::FileReader;
+
+struct Ingridient {
+    capacity: i32,
+    durability: i32,
+    flavor: i32,
+    texture: i32,
+    calories: i32,
+}
+
+// struct Cookie {
+//     capacity: i32,
+//     durability: i32,
+//     flavor: i32,
+//     texture: i32,
+//     calories: i32,
+// }
+
+const SUM: i32 = 100;
+const IDEAL_CALORIES: i32 = 500;
+
+fn main() {
+    let re = Regex::new(
+        r"^([^:]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)",
+    )
+    .unwrap();
+
+    let mut ingridients = Vec::new();
+
+    FileReader::process_lines("./input/problem_15.txt", &mut |line| {
+        let capture_match = re
+            .captures(line.trim())
+            .expect("Could not match the input line");
+
+        let parse_int = |idx: usize| capture_match[idx].parse::<i32>().unwrap();
+
+        // let ingridient_name = capture_match[1].to_string();
+
+        let capacity = parse_int(2);
+        let durability = parse_int(3);
+        let flavor = parse_int(4);
+        let texture = parse_int(5);
+        let calories = parse_int(6);
+
+        let ingridient = Ingridient {
+            capacity,
+            durability,
+            flavor,
+            texture,
+            calories,
+        };
+
+        ingridients.push(ingridient);
+
+        // println!("{ingridient_name}: {capacity}, {durability}, {flavor}, {texture}, {calories}");
+    });
+
+    let amount_of_ingridients = ingridients.len();
+    let all_combinations = vectors_with_fixed_sum(amount_of_ingridients, SUM);
+
+    let mut max_total_score = 0;
+    let mut max_total_score_with_calories = 0;
+
+    let ingridients = &ingridients;
+
+    for combination in all_combinations {
+        let mut total_capacity = 0;
+        let mut total_durability = 0;
+        let mut total_flavor = 0;
+        let mut total_texture = 0;
+        let mut total_calories = 0;
+
+        let enhanced_ingridients = combination.iter().zip(ingridients);
+
+        for (amount, ingridient) in enhanced_ingridients {
+            let Ingridient {
+                capacity,
+                durability,
+                flavor,
+                texture,
+                calories,
+            } = ingridient;
+
+            total_capacity += capacity * amount;
+            total_durability += durability * amount;
+            total_flavor += flavor * amount;
+            total_texture += texture * amount;
+            total_calories += calories * amount;
+        }
+
+        total_capacity = max(total_capacity, 0);
+        total_durability = max(total_durability, 0);
+        total_flavor = max(total_flavor, 0);
+        total_texture = max(total_texture, 0);
+
+        let total_score = total_capacity * total_durability * total_flavor * total_texture;
+
+        max_total_score = max(max_total_score, total_score);
+
+        if total_calories == IDEAL_CALORIES {
+            max_total_score_with_calories = max(max_total_score_with_calories, total_score);
+        }
+    }
+
+    println!("Max total score: {}", max_total_score); // 222870
+    println!(
+        "Max total score with {} calories: {}",
+        IDEAL_CALORIES, max_total_score_with_calories
+    );
+    // 117936
+
+    // for i in 0..100 {
+    //   let score =
+    //     for cookie in &ingridients {
+    //         let Ingridient {
+    //             capacity,
+    //             durability,
+    //             flavor,
+    //             texture,
+    //             calories,
+    //         } = cookie;
+
+    //      //   let score = i
+    //     }
+    // }
+}
+
+fn vectors_with_fixed_sum(length: usize, sum: i32) -> Vec<Vec<i32>> {
+    if length == 1 {
+        return vec![vec![sum]];
+    }
+
+    let mut result = Vec::new();
+
+    for i in 0..=sum {
+        let remaining_sum = sum - i;
+
+        let mut partial_result = vectors_with_fixed_sum(length - 1, remaining_sum);
+
+        for v in &mut partial_result {
+            v.push(i);
+        }
+
+        result.extend_from_slice(&partial_result);
+    }
+
+    return result;
+}
